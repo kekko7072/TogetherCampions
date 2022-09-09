5ed004f3e6fa02a143fba3b9a39b5fd0122229f8
kekko7072 <francesco.vezz01@gmail.com>
2022-09-09 17:50:35 +0200

    Aggiunta la connessione tramite porta seriale e inizio impelmentazione grafico della comunicazione tramite essa per eseguire operazioni di diagnosi e lettura/scrittura della SDCARD.

---
+++ cli/cli/lib/cli.dart
@@ -0,0 +1,9 @@
+#! /usr/bin/env dcli
+
+import 'dart:io';
+import 'package:dcli/dcli.dart';
+
+void main() {
+  var name = ask('name:', required: true, validator: Ask.alpha);
+  print('Hello $name');
+}
